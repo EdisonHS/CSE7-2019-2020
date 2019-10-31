63b56cac531e94075bf64281e7a5d7e4777d65f5
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2019-10-31 08:47:19 -0700

    Initial commit

---
+++ notes.py
@@ -0,0 +1,43 @@
+print("Hello World!")
+
+# Congrats! You just made your
+# first Python program!
+
+'''
+This is a multi-line comment
+Everything in between is ignored 
+by the program
+'''
+
+print(True)
+print(15)
+
+print("TRUE")
+print(9001)
+print('the thing')
+# print('the other thing")
+# print("Make sure quotes (" close ))
+print(7.14)
+
+# ------------
+# Math Section
+# ------------
+print()
+print()
+print("Math")
+print(3 + 4)  # Prints 7
+print(4 * 5)  # Prints 20
+print(5 - 2)  # Prints 3
+print(144 / 12)  # Prints 12
+
+# Powers
+print(3 ** 2)  # This should be 3^2
+print(2 ** 4)  # This should print 16
+
+print()
+print(9 // 3)  # Prints 3
+print(9 // 4)  # Prints 2
+print(8 // 3)  # Prints 2
+print(12 // 4)  # Prints 3
+print(11 // 4)  # Prints 2
+print(24 // 5)  # Print 4
