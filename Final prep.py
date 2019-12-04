927aa483a8ed9c4f321a2d348a8dab0a7bbe4a36
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2019-12-04 08:59:53 -0800

    Final prep

---
+++ Final prep.py
@@ -0,0 +1,52 @@
+"""
+What do you think will be on the final?
+Syntax
+inputs
+variables
+data types*
+Error Analysis
+Basic Prints
+Math Functions
+Exponents
+Floor division (//)
+Modulus (%)
+booleans (>, <, >=, !=, ==, etc)
+printing with variables
+recasting
+Escape Characters****
+rounding
+keywords (print, input, etc)
+concatenation
+
+"""
+
+# Basic prints
+print("Hello World")
+
+# Basic Math
+print(5 + 3)
+print(13241651341 * 2356)
+print(155 / 23)
+
+# Exponents
+print(52 ** 2)  # This is 52 to the power of 2
+print(25 ** (1/2))  # This is the square root of 25
+
+# Floor Division
+print(11 // 4)  # This divides and rounds down
+
+# Escape Characters
+# THIS IS THE NEW ONE!!!!!!
+print("The cow goes \"Moo\"")
+print('Welcome to Mr Wiebe\'s class')
+print("This is on one line. \nThis is on the other.")
+print("\tThis is a tab!")
+print("AM\\PM")
+
+
+print("\tIndent this. \nPrint this on a new line.")
+
+# Rounding
+number = 5.29234634354313543246843216546543246
+print("This is about {:.2f}".format(number))
+print("This is about {:.7f}".format(number))
--- Inputs Excercises.py
+++ Inputs Excercises.py
@@ -1,1 +1,12 @@
 # Challenge 1
+
+
+# ------
+second_number = int(input("asdf"))
+print("Your age is {}".format(age))
+print("You typed in {}".format(name))
+
+area = length*width
+print("{}".format(area))
+
+print("{} {}".format(length, width))
